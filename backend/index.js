0263c85ac0a7325f37263ccffbfd0cdfab55ee5e
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2025-12-20 21:34:26 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const connectionString = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(connectionString).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+/*Vv0NTmQ6Yx9iVvIu
+yehanjb_db_user
+*/
