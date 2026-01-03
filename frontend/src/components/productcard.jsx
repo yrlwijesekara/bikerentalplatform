4e751c94006c127336eaf0cc2886677e4fbe7a11
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2026-01-03 20:09:21 +0530

    feat: update frontend dependencies and integrate Tailwind CSS
    - Added Tailwind CSS and related dependencies to package.json
    - Removed old CSS styles from App.css and replaced with Tailwind CSS import in index.css
    - Refactored App.jsx to utilize ProductCard component and simplified layout
    - Created ProductCard component for product display
    - Updated vite.config.js to include Tailwind CSS plugin
    - Added .gitignore for backend to exclude unnecessary files

---
+++ frontend/src/components/productcard.jsx
@@ -0,0 +1,12 @@
+function ProductCard() {
+  return (
+    <div className="product-card">
+      <h1>Name</h1>
+      <img src="image-url" alt="Product Image" />
+      <p>Price</p>
+      <button>View Details</button>
+    </div>
+  );
+}
+
+export default ProductCard;
