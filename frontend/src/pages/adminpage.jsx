9ff49794b465e88fda2510460ffb4cf6ef1683d3
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2026-01-05 11:50:25 +0530

    enhance admin panel layout and add hover effects to links

--- frontend/src/pages/adminpage.jsx
+++ frontend/src/pages/adminpage.jsx
@@ -13,19 +13,19 @@ export default function Adminpage() {
         <div className="w-full h-screen flex">
             <div className="w-[400px] h-full bg-amber-900 flex flex-col items-center">
                 
-                <div className="w-full m-6 p-6 ">
-                <h1 className="text-white text-4xl font-bold m-4">Admin Panel</h1>
-                     <Links to="/admin" className="w-full block p-4 text-white border-b border-amber-700 text-2xl">
+                <div className="w-full m-6 gap-5 flex flex-col">
+                <h1 className="text-white text-4xl font-bold m-4 flex justify-center items-center">Admin Panel</h1>
+                     <Links to="/admin" className="w-full flex justify-center items-center p-4 text-white border-b border-amber-700 text-2xl transition-all duration-300 hover:cursor-pointer hover:scale-105 hover:bg-amber-800 hover:shadow-lg">
                     <VscGraph className="inline-block mr-2" /> Dashboard</Links>
-                <Links to="/admin/products" className="block p-4 text-white border-b border-amber-700 text-2xl" >
+                <Links to="/admin/products" className="flex justify-center items-center p-4 text-white border-b border-amber-700 text-2xl transition-all duration-300 hover:cursor-pointer hover:scale-105 hover:bg-amber-800 hover:shadow-lg" >
                     <RiMotorbikeFill className="inline-block mr-2" /> Products</Links>
-                <Links to="/admin/users" className="block p-4 text-white border-b border-amber-700 text-2xl">
+                <Links to="/admin/users" className="flex justify-center items-center p-4 text-white border-b border-amber-700 text-2xl transition-all duration-300 hover:cursor-pointer hover:scale-105 hover:bg-amber-800 hover:shadow-lg">
                     <FaUserAlt className="inline-block mr-2" /> Users</Links>
-                <Links to="/admin/vendors" className="block p-4 text-white border-b border-amber-700 text-2xl">
+                <Links to="/admin/vendors" className="flex justify-center items-center p-4 text-white border-b border-amber-700 text-2xl transition-all duration-300 hover:cursor-pointer hover:scale-105 hover:bg-amber-800 hover:shadow-lg">
                     <TiVendorAndroid className="inline-block mr-2" /> Vendors</Links>
-                <Links to="/admin/bookings" className="block p-4 text-white border-b border-amber-700 text-2xl">
+                <Links to="/admin/bookings" className="flex justify-center items-center p-4 text-white border-b border-amber-700 text-2xl transition-all duration-300 hover:cursor-pointer hover:scale-105 hover:bg-amber-800 hover:shadow-lg">
                     <FaBookmark className="inline-block mr-2" /> Bookings</Links>
-                <Links to="/admin/reviews" className="block p-4 text-white border-b border-amber-700 text-2xl">
+                <Links to="/admin/reviews" className="flex justify-center items-center p-4 text-white border-b border-amber-700 text-2xl transition-all duration-300 hover:cursor-pointer hover:scale-105 hover:bg-amber-800 hover:shadow-lg">
                     <VscCodeReview className="inline-block mr-2" /> Reviews</Links></div>
            
             </div>
