80070c93f2d809f8c51ab0244eadfa948579a1ce
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2026-01-24 14:07:29 +0530

    refactor Clientpage: define client-specific routes and enhance structure for better readability🚀

--- frontend/src/pages/client/clientpage.jsx
+++ frontend/src/pages/client/clientpage.jsx
@@ -1,12 +1,19 @@
 import { Routes } from "react-router-dom";
 import Header from "../../components/header";
+import { Route } from "react-router-dom";
+import Notfoundpage from "../notfound.jsx";
 
 export default function Clientpage() {
   return (
     <div className="w-full h-screen max-h-screen bg-[#F5F5F5]">
         <Header />
-        <Routes>
-            {/* Define client-specific routes here */}
+        <Routes path="/">
+            <Route path="/" element={<h1 className="text-black">Client Home Page</h1>} />
+            <Route path="browse-bikes" element={<h1 className="text-black">Browse Bikes Page</h1>} />
+            <Route path="about-sri-lanka" element={<h1 className="text-black">About Sri Lanka Travel Page</h1>} />
+            <Route path="/*" element={<Notfoundpage />} />
+            
+            
         </Routes>
     </div>
   );
